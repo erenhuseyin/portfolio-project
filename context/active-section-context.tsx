5e4cbdb4184cc1618a505e112072838a988663e6
huseyineren <huseyin.eren@finastech.com>
2024-08-16 01:13:44 +0300

    bugfix

--- context/active-section-context.tsx
+++ context/active-section-context.tsx
@@ -20,7 +20,7 @@ export const ActiveSectionContext =
 export default function ActiveSectionContextProvider({
   children,
 }: ActiveSectionContextProviderProps) {
-  const [activeSection, setActiveSection] = useState<SectionName>("Home");
+  const [activeSection, setActiveSection] = useState<SectionName>("Giriş");
   const [timeOfLastClick, setTimeOfLastClick] = useState(0); // we need to keep track of this to disable the observer temporarily when user clicks on a link
 
   return (
